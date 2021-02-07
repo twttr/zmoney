67a58f46ff9dca017c95f0af86e7e2307a201ad6
Pavel Romanishkin <me@twttr.io>
2021-02-07 16:52:16 +0100

    Initial Commit

---
+++ zmoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zmoney
+//
+//  Created by Pavel Romanishkin on 07.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
