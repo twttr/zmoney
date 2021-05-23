fccc79f12919f456855c2ecdb0cc2c65f76cb335
Pavel Romanishkin <me@twttr.io>
2021-05-23 19:32:47 +0200

    revise detailed transaction view

--- zmoney/Controllers/TransactionDetailViewController.swift
+++ zmoney/Controllers/TransactionDetailViewController.swift
@@ -11,14 +11,24 @@ class TransactionDetailViewController: UIViewController {
     @IBOutlet weak private var amountLabel: UILabel!
     @IBOutlet weak private var accountLabel: UILabel!
     @IBOutlet weak private var categoryLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var payeeLabel: UILabel!
+    @IBOutlet weak var purchaseImageView: UIImageView!
+    @IBOutlet weak var purchaseView: UIView!
+    @IBOutlet weak var currencyLabel: UILabel!
 
     var transactionCellModel: TransactionCellModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        purchaseView.layer.cornerRadius = Constants.Buttons.cornerRadius
+
         amountLabel.text = transactionCellModel?.amount
         accountLabel.text = transactionCellModel?.account
         categoryLabel.text = transactionCellModel?.categories.joined(separator: ", ")
+        dateLabel.text = transactionCellModel?.date
+        payeeLabel.text = transactionCellModel?.payee
+        currencyLabel.text = transactionCellModel?.currency
     }
 }
